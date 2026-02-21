8651c3b2f8d6853fc3f03082f4953fcfd9336ab3
Erica Avendaño <ericamarcelaavendano@gmail.com>
2026-02-21 14:03:32 -0500

    Creacion de clase person, incluye getter, setter y constructores

---
+++ src/main/java/co/edu/cesde/g/moodels/Person.java
@@ -0,0 +1,44 @@
+package co.edu.cesde.g.moodels;
+
+public class Person {
+    //Aplicaciòn de atributos.
+    long user_Id;
+    String code;
+    String documentNumber;
+    String FirstName;
+    String LastName;
+    Boolean status;
+
+    public long getUser_Id() {
+        return user_Id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public Person(long user_Id, String code, String documentNumber, String firstName, String lastName, Boolean status) {
+        this.user_Id = user_Id;
+        this.code = code;
+        this.documentNumber = documentNumber;
+        FirstName = firstName;
+        LastName = lastName;
+        this.status = status;
+    }
+}
