8bffa2fd410b3263222d6440ddbbad913088488e
Erica Avendaño <ericamarcelaavendano@gmail.com>
2026-03-10 21:27:54 -0500

    Creacion de la clase UserRole y sus atributos

---
+++ src/main/java/co/edu/cesde/g/models/UserRole.java
@@ -0,0 +1,38 @@
+package co.edu.cesde.g.models;
+
+public class UserRole {
+    private Long userId;
+    private Long roleId;
+
+    public UserRole() {
+    }
+
+    public UserRole(Long userId, Long roleId) {
+        this.userId = userId;
+        this.roleId = roleId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "userId=" + userId +
+                ", roleId=" + roleId +
+                '}';
+    }
+}
+
